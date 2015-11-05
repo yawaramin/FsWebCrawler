913ac5d3b5fc2c4dfb69339006c691cb88430d4b
Yawar Amin <yawar.amin@gmail.com>
2015-11-04 23:17:55 -0500

    Add doc comments

--- FsWebCrawler/Program.fs
+++ FsWebCrawler/Program.fs
@@ -2,6 +2,18 @@
 open System.Net
 open System.Text.RegularExpressions
 
+(**
+<summary>Crawl a single page and return all links found
+therein.</summary>
+
+<param name="client">The WebClient object to use to crawl the
+page.</param>
+
+<param name="pageUrl">The URL of the page to crawl.</param>
+
+<returns>A list of URLs which are links found in the crawled
+page.</returns>
+*)
 let crawlPage (client : WebClient) (pageUrl : string) =
   try
     let pageContent = client.DownloadString(pageUrl)
@@ -18,6 +30,23 @@ let crawlPage (client : WebClient) (pageUrl : string) =
 
   with | _ -> []
 
+(**
+<summary>Crawl a given site to a given depth and print out the links
+found therein.</summary>
+
+<param name="client">The WebClient object to use to crawl the
+site.</param>
+
+<param name="curDepth">The depth at which we are currently crawling
+relative to the initial site URL at which we started crawling.</param>
+
+<param name="moreDepth">How many more levels of links to continue
+crawling.</param>
+
+<param name="siteUrl">The URL of the site to crawl.</param>
+
+<returns>Nothing.</returns>
+*)
 let rec crawlSite client curDepth moreDepth siteUrl =
   if moreDepth > 0
     then
@@ -28,10 +57,20 @@ let rec crawlSite client curDepth moreDepth siteUrl =
         printfn "%s %s" prefix link
         crawlSite client (curDepth + 1) (moreDepth - 1) link
 
+(**
+<summary>Crawl a given site to a given depth and print out the links
+found therein.</summary>
+
+<returns>Nothing.</returns>
+*)
 let crawl depth siteUrl =
   let client = new WebClient()
   crawlSite client 0 depth siteUrl
 
+(**
+Main function that drives the app. This function handles the given
+command-line arguments and makes sure they're valid.
+*)
 [<EntryPoint>]
 let main argv =
   let usage () =
